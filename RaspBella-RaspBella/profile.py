ebb77df254eeb439b0795abe545fb0bdb5a31a96
RaspBella <bencampbell3233@gmail.com>
2022-08-23 22:39:17 +0100

    Removed unnecessary code to display in profile.py, and removed the c files because I am going to just make bella.c(maybe some header{s,}) eventually

--- RaspBella-RaspBella/profile.py
+++ RaspBella-RaspBella/profile.py
@@ -6,8 +6,7 @@ class OS():
 
 class Computer():
     def __init__(self, OS):
-        self.OS = OS
-            
+        self.OS = OS            
 
 class Programmer():
     def __init__(self, name, age, pronouns, favourite_languages, desktop, laptop):
@@ -17,30 +16,6 @@ class Programmer():
         self.favourite_languages = favourite_languages
         self.desktop = desktop
         self.laptop = laptop
-
-    def display(self):
-        print("Name:", self.name+"\nAge:", str(self.age)+"\nPronouns:", end=" ")
-        print(*self.pronouns, sep=', ')
-        print("Favourite languages:", end=" ")
-        print(*self.favourite_languages, sep=', ')
-        print("Desktop:")
-        for x in self.desktop.OS:
-            print("\t"+x+":")
-            if len(self.desktop.OS[x].DE) > 0:
-                print("\t\tDE: "+self.desktop.OS[x].DE)
-            if len(self.desktop.OS[x].browser) > 0:
-                print("\t\tBrowser: "+self.desktop.OS[x].browser)
-            if len(self.desktop.OS[x].IDE) > 0:
-                print("\t\tIDE: "+self.desktop.OS[x].IDE)
-        print("Laptop:")
-        for x in self.laptop.OS:
-            print("\t"+x+":")
-            if len(self.laptop.OS[x].DE) > 0:
-                print("\t\tDE: "+self.laptop.OS[x].DE)
-            if len(self.laptop.OS[x].browser) > 0:
-                print("\t\tBrowser: "+self.laptop.OS[x].browser)
-            if len(self.laptop.OS[x].IDE) > 0:
-                print("\t\tIDE: "+self.laptop.OS[x].IDE)
 
 Me = Programmer(
     "Bella Campbell",
@@ -61,5 +36,3 @@ Me = Programmer(
         }
     )
 )
-
-Me.display()
